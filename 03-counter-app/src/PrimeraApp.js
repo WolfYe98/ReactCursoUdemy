6acf3679300c2c48537035d79ac0b2eccf88d7da
Bate Ye <mingye@hotmail.es>
2021-12-29 18:17:38 +0100

    Created new ReactProject

---
+++ 03-counter-app/src/PrimeraApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PrimeraApp =()=>{ // esto ya cuenta como un functional component.
+    return(
+        <>
+            <h1>Hola mundo</h1> 
+            <p>Hola soy p</p>
+        </>
+    );
+};
+
+export default PrimeraApp;
