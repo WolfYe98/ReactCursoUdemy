c6de306f26725aba22b4ae55b427d2def11aa0f0
Bate Ye <mingye@hotmail.es>
2021-12-30 13:03:02 +0100

    Terminado seccion useState()

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React,{useState} from 'react';
 import PropTypes from 'prop-types';
-const CounterApp=({value})=>{
-    const handledAdd =(e)=>{
-        console.log(e)
+
+const CounterApp=({value = 10})=>{
+    const [counter,setCounter] = useState(value);
+
+    const handledAdd =(e,val)=>{
+        setCounter(counter+val);
+    }
+    const reset = ()=>{
+        setCounter(value);
     }
     return(
         <>
             <h1>CounterApp</h1>
-            <h2> { value } </h2>
-            <button onClick={handledAdd}>Click Me!</button>
+            <h2> { counter } </h2>
+            <button onClick={e=>handledAdd(e,1)}>+1</button>
+            <button onClick={reset}>Reset</button>
+            <button onClick={e=>handledAdd(e,-1)}>-1</button>
         </>
     );
 };
 CounterApp.propTypes={
-    value:PropTypes.number.isRequired
+    value:PropTypes.number
 };
 
 export default CounterApp;
